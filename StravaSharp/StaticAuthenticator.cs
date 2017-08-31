2e33d5bba93043ef34754da69dbd5d8a6c5d920c
Alexey Milokhov <milokhov@live.com>
2017-08-31 13:17:36 -0400

    StaticAuthenticator added

---
+++ StravaSharp/StaticAuthenticator.cs
@@ -0,0 +1,49 @@
+﻿using RestSharp.Portable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StravaSharp
+{
+    public class StaticAuthenticator : IAuthenticator
+    {
+        public StaticAuthenticator(string accessToken)
+        {
+            AccessToken = accessToken;
+        }
+
+        public string AccessToken { get; }
+
+        public bool CanHandleChallenge(IHttpClient client, IHttpRequestMessage request, ICredentials credentials, IHttpResponseMessage response)
+        {
+            return false;
+        }
+
+        public bool CanPreAuthenticate(IRestClient client, IRestRequest request, ICredentials credentials)
+        {
+            return true;
+        }
+
+        public bool CanPreAuthenticate(IHttpClient client, IHttpRequestMessage request, ICredentials credentials)
+        {
+            return true;
+        }
+
+        public Task HandleChallenge(IHttpClient client, IHttpRequestMessage request, ICredentials credentials, IHttpResponseMessage response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task PreAuthenticate(IRestClient client, IRestRequest request, ICredentials credentials)
+        {
+            request.AddHeader("Authorization", "Bearer " + AccessToken);
+        }
+
+        public async Task PreAuthenticate(IHttpClient client, IHttpRequestMessage request, ICredentials credentials)
+        {
+            request.Headers.Add("Authorization", "Bearer " + AccessToken);
+        }
+    }
+}
